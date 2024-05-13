9609b62779f485467a5312329c983ec7f66cf23f
Otamurod <otamurodpirnapasov2003@gmail.com>
2024-05-13 09:16:34 +0500

    changes loader js

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ function about() {
   alert("Ma'lumotlar yaqin kunlarda joylanadi, e'tiboringiz uchun rahmat !!!");
 }
 // Sahifa to'liq yuklansa
-window.addEventListener('load', function () {
-    // Loader div elementini tanlash
-    var loaderContainer = document.getElementById('loader-container');
+// window.addEventListener('load', function () {
+//     // Loader div elementini tanlash
+//     var loaderContainer = document.getElementById('loader-container');
 
-    this.setTimeout(function(){
-        // Loader div elementini yashirish
-        loaderContainer.style.display="none";
-    })
+//     this.setTimeout(function(){
+//         // Loader div elementini yashirish
+//         loaderContainer.style.display="none";
+//     })
 
-});
+// });
 
